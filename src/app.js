55e618fc495b51dda2bc76313d790ffb40aaeb70
Arahanth-M <151705727+Arahanth-M@users.noreply.github.com>
2024-11-03 19:27:29 +0530

    middle wares and request handlers

--- src/app.js
+++ src/app.js
@@ -41,33 +41,54 @@ const app = express(); //new webserver is created
 
 
 
-app.use("/user", [(req, res, next) => {
-        console.log("Routing handeled by route handler 1");
-        //res.send("response from route handler 1");
-        next();
+// app.use("/user", [(req, res, next) => {
+//         console.log("Routing handeled by route handler 1");
+//         //res.send("response from route handler 1");
+//         next();
 
-    },
-    (req, res, next) => {
-        console.log("Routing handeled by route handler 2");
-        //res.send("response from route handler 2");
-        next();
-    },
-    (req, res, next) => {
-        console.log("Routing handeled by route handler 3");
-        //res.send("response from route handler 3");
-        next();
-    },
-    (req, res, next) => {
-        console.log("Routing handeled by route handler 4");
-        res.send("response from route handler 4");
-        // next();
-    },
-    (req, res, next) => {
-        console.log("Routing handeled by route handler 5");
-        res.send("response from route handler 5");
+//     },
+//     (req, res, next) => {
+//         console.log("Routing handeled by route handler 2");
+//         //res.send("response from route handler 2");
+//         next();
+//     },
+//     (req, res, next) => {
+//         console.log("Routing handeled by route handler 3");
+//         //res.send("response from route handler 3");
+//         next();
+//     },
+//     (req, res, next) => {
+//         console.log("Routing handeled by route handler 4");
+//         res.send("response from route handler 4");
+//         next();
+//     },
+//     (req, res, next) => {
+//         console.log("Routing handeled by route handler 5");
+//         // res.send("response from route handler 5");
 
-    }
-]);
+//     }
+
+
+// ]);
+
+//this is also  avalid syntax , all the middle fucntions tah tdo not actually respond and lead to other function are known as middle wares
+//the function that actually reply with the rsponse are called as request handlers
+
+app.use("/", (req, res, next) => {
+    console.log("handeled by / router");
+    next();
+});
+
+app.use("/user", (req, res, next) => {
+        console.log("handeled by the /user router 1");
+        //res.send("response from router 2");
+        next();
+    },
+    (req, res, next) => {
+        console.log("handeled by the /user router 2");
+        res.send("response from the router 3");
+        next();
+    })
 
 
 // the array that we ave used can be used or not used or used between any function s , anything u can do is fine 
