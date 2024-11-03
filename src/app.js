51bb408a48b2b7be1b35537a48114d6be3590afa
Arahanth-M <151705727+Arahanth-M@users.noreply.github.com>
2024-11-03 16:19:03 +0530

    mad use of HTTP methods on postman

--- src/app.js
+++ src/app.js
@@ -2,16 +2,31 @@ const express = require("express");
 
 const app = express(); //new webserver is created
 
-// app.use((req, res) => {
-//     res.send("hello from the server");
-// }) // the function we have passed insdie is called as arequest handler
 
-app.use("/hello", (req, res) => {
-    res.send("hello from the server usinf dev - home page");
-})
 
+
+//app.use will match all HHTP method call test
 app.use("/test", (req, res) => {
     res.send("hello from the server using nodemon npx - test page");
+})
+
+app.get("/user", (req, res) => {
+    res.send({
+        firstname: "Arahanth",
+        lastname: "M"
+    })
+})
+
+app.post("/user", (req, res) => {
+    console.log("save the data in the database");
+    res.send("database successfully updated");
+
+})
+
+app.delete("/user", (req, res) => {
+    //console.log("save the data in the database");
+    res.send("user deleted successfully");
+
 })
 
 
