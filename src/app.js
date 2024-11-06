03a11cef67829f76a0885017576730b2acb0b90a
Arahanth-M <151705727+Arahanth-M@users.noreply.github.com>
2024-11-06 17:46:31 +0530

    Schema updated and data santization done

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
-const express = require("express");
+const express = require("express"); //including the core node module onto our app.js code
 const {
     connectDB
-} = require("./config/database");
+} = require("./config/database"); //including our database module... from the config folder
 
 
 
-const User = require("./models/user");
+const User = require("./models/user"); // we have created a schema , then a model of user dataabase , we are trying to import it here
+
 
 app = express();
 
-app.use(express.json()); //middle ware used to convert the JSON format to JS object , so that it is in user readable form 
+app.use(express.json()); //middle ware used to convert the JSON format to JS object , so that it is in user readable form and fetch only the required data to the server
+
 
 app.post("/signup", async (req, res) => {
     console.log(req.body);
@@ -20,11 +22,21 @@ app.post("/signup", async (req, res) => {
         res.send("user data updated successfully");
     } catch (err) {
         console.log("error occured");
-        res.status(404).send("could not recieve the data");
+        res.status(404).send(err.message);
     }
 
 
 });
+
+// app.post("/user", async (req, res) => {
+//     const user = new User(req.body);
+//     try {
+//         await user.save();
+//         res.send("user data updated successfully");
+//     } catch (err) {
+//         req.status(404).send("some unknown error occured");
+//     }
+// })
 
 app.get("/user", async (req, res) => {
     const userEmail = req.body.email;
@@ -47,6 +59,32 @@ app.get("/user", async (req, res) => {
 
 })
 
+app.delete("/delete", async (req, res) => {
+    const userEmail = req.body.email;
+    try {
+        await User.deleteOne({
+            email: userEmail
+        });
+        res.send("user deleted successfully");
+
+
+    } catch (err) {
+        res.status(404).send("some error occured");
+    }
+})
+//using findByIdAnddelete() method
+app.delete("/user", async (req, res) => {
+    const userid = req.body.userId;
+    try {
+        await User.findByIdAndDelete({
+            userId: userid
+        })
+        res.send("user successfully deleted using id");
+    } catch (err) {
+        res.send("some unknown error occured");
+    }
+})
+
 app.get("/feed", async (req, res) => {
     try {
         const users = await User.find({});
@@ -64,6 +102,29 @@ app.get("/feed", async (req, res) => {
 
 
 })
+
+app.patch("/user", async (req, res) => {
+    const userId = req.body.userId;
+    const data = req.body;
+    try {
+        const user = await User.findByIdAndUpdate({
+                _id: userId
+            },
+            data, {
+                returnDocument: "after",
+                runValidators: true,
+            }
+        );
+        console.log(user);
+        res.send("User data updated successfully");
+    } catch (err) {
+        res.status(404).send("some unknown err occured" + err);
+    }
+
+
+})
+
+
 
 connectDB().then(() => {
     console.log("connected to the database");
--- src/models/user.js
+++ src/models/user.js
@@ -3,27 +3,63 @@ const mongoose = require("mongoose");
 //creating  a schema
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 4,
+        maxLength: 50,
+
     },
     lastName: {
-        type: String
+        type: String,
+
+
     },
     email: {
-        type: String
+        type: String,
+        lowercase: true,
+        required: true,
+        unique: true,
+        index: true,
+        trim: true
     },
     password: {
-        type: String
+        type: String,
+        required: true,
     },
     age: {
-        type: Number
+        type: Number,
+        min: 18,
+        max: 100
     },
     gender: {
-        type: String
+        type: String,
+        //validate functions by defalut works only when a new document was created , will not work whenever a document is getting updated
+        //inorder to run the validators , u should pass runValidators along with the options in the  patch API of the users
+        //very important concept and very few developers will be knowing about this...
+        //you can check the patch API for updating the user value in the app.js code for better clarity 
+        validate(value) {
+            if (!["male", "female", "others"].includes(value))
+                throw new Error("Please enter a valid gender!!!");
+        }
+    },
+    photoUrl: {
+        type: String,
+        default: "https://i.pinimg.com/564x/8a/01/cc/8a01cc0579be056ecc8dfa2f07bd42aa.jpg",
+    },
+    about: {
+        type: String,
+        default: "add description about yourself",
+    },
+    skills: {
+        type: [String],
     }
 
 
 
 
+
+}, {
+    timestamps: true,
 })
 
 //creating a model
